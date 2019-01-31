86145ff0b2ec2273039e25980a8f1c8c52b973e5
wangzic95 <403466144@qq.com>
2019-01-31 10:43:55 +0800

    主面改版

---
+++ static/common/js/app.js
@@ -0,0 +1,10 @@
+var jsAryList =[
+    "static/uiframe/bootstrap4/js/jquery.min.js"
+    ,"static/jquery/jquery.slim.min.js"
+    ,"static/uiframe/bootstrap4/js/bootstrap.min.js"
+    ,"static/uiframe/bootstrap4/js/popper.min.js"
+    ,"static/uiframe/bootstrap4/js/holder.min.js"
+];
+for(var i=0;i<jsAryList.length;i++){
+    document.write("<script language=javascript src='"+jsAryList[i]+"'></script>");
+}
